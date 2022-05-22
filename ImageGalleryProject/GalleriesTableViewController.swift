86621ea97d6d2453db2d391d6fffb13dc54dc265
Or <or.healthy@healthy.io>
2022-05-22 12:08:13 +0300

    Table View With 2 sections added

---
+++ ImageGalleryProject/GalleriesTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  GalleriesTableViewController.swift
+//  ImageGalleryProject
+//
+//  Created by Or Peleg on 22/05/2022.
+//
+
+import UIKit
+
+class GalleriesTableViewController: UITableViewController {
+    
+    var sections: [[String]] = [[]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sections = [[], ["Recently Deleted"]]
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.sections[section].count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GalleryCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = sections[indexPath.section][indexPath.row]
+        return cell
+    }
+
+    @IBAction func addNewGallery(_ sender: UIBarButtonItem) {
+        if !sections[0].contains("Untitled") {
+            sections[0] += ["Untitled"]
+        } else {
+            let untitledCell = "Untitled"
+            var numberOfUntitledCell = 1
+            var itemThatExists = untitledCell + " " + String(numberOfUntitledCell)
+            while sections[0].contains(itemThatExists)
+            {
+                numberOfUntitledCell += 1
+                itemThatExists = untitledCell + " " + String(numberOfUntitledCell)
+            }
+            sections[0] += [itemThatExists]
+        }
+        tableView.reloadData()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if splitViewController?.preferredDisplayMode != UISplitViewController.DisplayMode.oneOverSecondary {
+            splitViewController?.preferredDisplayMode = UISplitViewController.DisplayMode.oneOverSecondary
+        }
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            if editingStyle == .delete {
+                sections[0].remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } else if editingStyle == .insert {
+                // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle
+    {
+        if indexPath.section == 1 {
+            return UITableViewCell.EditingStyle.none
+        } else {
+            return UITableViewCell.EditingStyle.delete
+        }
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
